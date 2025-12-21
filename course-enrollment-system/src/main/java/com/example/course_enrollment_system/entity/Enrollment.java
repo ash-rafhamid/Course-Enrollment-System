c37aa6842c0591adee6c9bb0a6e4290aa39a741a
ash-rafhamid <ashrafhamidmajumder@gmail.com>
2025-12-21 18:41:29 +0600

    Worked with enrollement entity table  added uniqueconstraint

--- course-enrollment-system/src/main/java/com/example/course_enrollment_system/entity/Enrollment.java
+++ course-enrollment-system/src/main/java/com/example/course_enrollment_system/entity/Enrollment.java
@@ -1,9 +1,6 @@
 package com.example.course_enrollment_system.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
-import jakarta.persistence.ManyToOne;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -14,15 +11,18 @@ import java.time.LocalDate;
 @Setter
 @Getter
 @NoArgsConstructor
+@Table(name = "enrollments",uniqueConstraints = @UniqueConstraint(columnNames = {"student_id", "course_id"}))
 public class Enrollment {
     @Id
     @GeneratedValue(strategy = jakarta.persistence.GenerationType.IDENTITY)
     private Long id;
 
     @ManyToOne
+    @JoinColumn(name = "student_id")
     private Student student;
 
     @ManyToOne
+    @JoinColumn(name = "course_id")
     private Course course;
 
     private LocalDate date;
